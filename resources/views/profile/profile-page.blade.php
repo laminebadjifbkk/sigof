0d9872024b249addd4765050a7da0e2c821599f4
elhadji907 <badjilaminefbkk@gmail.com>
2025-03-29 12:01:16 +0000

    update profil

--- resources/views/profile/profile-page.blade.php
+++ resources/views/profile/profile-page.blade.php
@@ -79,7 +79,7 @@
                     </div>
                 </div>
 
-                @role('Employe')
+                @role('Demandeur')
                     <div class="col-12 col-md-12 col-lg-12 col-sm-12 col-xs-12 col-xxl-12">
                         <div class="card">
                             <div class="card-body pb-0">
