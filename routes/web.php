41e9fe5ec70d9d88d8ea18376cb7d0a57e9c5b83
elhadji907 <badjilaminefbkk@gmail.com>
2025-04-01 18:53:56 +0000

    update book manuel view

--- routes/web.php
+++ routes/web.php
@@ -568,7 +568,7 @@ Route::group(['middleware' => ['XSS']], function () {
         Route::resource('/commissionmembres', CommissionmembreController::class);
 
         Route::middleware('admin')->group(function () {
-            Route::get('/books', [BookController::class, 'index'])->name('books');
+            Route::get('/manuels', [BookController::class, 'index'])->name('books');
             Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
             Route::post('/books', [BookController::class, 'store'])->name('books.store');
             Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
