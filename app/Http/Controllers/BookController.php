06444056cb92e3db5c1864517ad1f9b5285426ce
elhadji907 <badjilaminefbkk@gmail.com>
2025-04-01 18:54:39 +0000

    update book manuel view

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -17,7 +17,7 @@ class BookController extends Controller
     {
         $books = Book::all();
 
-        return view('books.indexes', compact('books'));
+        return view('books.index', compact('books'));
     }
 
     public function create()
