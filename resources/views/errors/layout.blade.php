7ea08c37de1189d0a138ab8ae7ed3b9818cc8cd7
elhadji907 <badjilaminefbkk@gmail.com>
2025-04-04 22:31:36 +0000

    update errors views

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('assets/img/favicon-onfp.png') }}" rel="icon">
     <title>Erreur @yield('code') - @yield('title')</title>
     <style>
         body {
