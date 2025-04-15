17b69218154f7ba7f679cc175f516e426dfde053
elhadji907 <badjilaminefbkk@gmail.com>
2025-04-15 22:29:11 +0000

    update

--- resources/views/courriers/arrives/index.blade.php
+++ resources/views/courriers/arrives/index.blade.php
@@ -139,7 +139,7 @@
                                 <thead>
                                     <tr>
                                         <th class="text-center" width='8%'>N° arrivé</th>
-                                        <th class="text-center">Date arrivé</th>
+                                        <th class="text-center"width='8%'>Date arrivé</th>
                                         {{-- <th class="text-center">N° corres.</th> --}}
                                         {{-- <th class="text-center">Date corres.</th> --}}
                                         <th>Expéditeur</th>
