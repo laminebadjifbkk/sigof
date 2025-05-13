29d737a6d9de01013f40c332602357beeb171c21
elhadji907 <badjilaminefbkk@gmail.com>
2025-05-13 11:50:51 +0000

    update

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -390,10 +390,10 @@
                                         <div class="col-12 col-md-12 col-lg-12 col-sm-12 col-xs-12 col-xxl-12">
                                             <div class="form-group">
                                                 <label for="telephone" class="form-label">Téléphone</label>
-                                                <input minlength="5" maxlength="10" type="text" name="telephone"
-                                                    value="{{ old('telephone') }}"
+                                                <input name="telephone" type="text" maxlength="12"
                                                     class="form-control form-control-sm @error('telephone') is-invalid @enderror"
-                                                    id="telephone" placeholder="7xxxxxxxx">
+                                                    id="telephone" value="{{ old('telephone', $user->telephone ?? '') }}"
+                                                    autocomplete="tel" placeholder="XX:XXX:XX:XX">
                                                 @error('telephone')
                                                     <span class="invalid-feedback" role="alert">
                                                         <div>{{ $message }}</div>
