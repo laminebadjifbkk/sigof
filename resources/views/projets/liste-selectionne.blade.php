6fc629592e2c40e9f7d0f0e83136173c7c6502a0
elhadji907 <badjilaminefbkk@gmail.com>
2025-05-20 08:48:41 +0000

    update execquo compte

--- resources/views/projets/liste-selectionne.blade.php
+++ resources/views/projets/liste-selectionne.blade.php
@@ -75,85 +75,6 @@
         </b>
     </div>
     <div class="invoice-box">
-        {{-- <table class="table table-responsive">
-            <thead>
-                <tr class="heading" style="text-align: center;">
-                    <td colspan="11"><b>{{ __('LISTE DES CANDIDATS SELECTIONNES') }}</b>
-                    </td>
-                </tr>
-                <tr class="heading">
-                    <td colspan="5"><b>{{ $projet?->type_projet }}</b>
-                        {{ $projet?->sigle }} - {{ $projet?->name }}
-                    </td>
-                    <td colspan="6"><b>{{ __('Module : ') }}</b> {{ $projetmodule->module }}
-                    </td>
-                </tr>
-                <tr class="heading">
-                    <td colspan="5"><b>{{ __('Lieu: ') }}</b> {{ $formation?->lieu }}
-                    </td>
-                    <td colspan="6"><b>{{ __('Opérateur: ') }}</b>
-                        {{ $formation?->operateur?->user?->operateur . ' (' . $formation?->operateur?->user?->username . ')' }}
-                    </td>
-                </tr>
-
-                <tr class="heading">
-                    <td colspan="2"><b>{{ __('Code formation : ') }}</b>
-                        {{ $formation?->code }}
-                    </td>
-                    <td colspan="3"><b>{{ __('Niveau de qualification : ') }}</b>
-                        @if ($formation?->type_certification !== 'Titre')
-                            {{ $formation?->titre ?? $formation?->referentiel?->titre }}
-                        @else
-                            @if (!empty($formation?->referentiel?->categorie))
-                                {{ $formation?->referentiel?->categorie . ' de la ' . $formation?->referentiel?->convention?->name }}
-                            @endif
-                        @endif
-                    </td>
-                    <td colspan="6"><b>{{ __('Titre : ') }}</b>
-                        @if ($formation?->type_certification !== 'Titre')
-                            {{ $formation?->type_certification }}
-                        @else
-                            {{ $formation?->referentiel?->titre }}
-                        @endif
-                    </td>
-                </tr>
-                <tr class="heading">
-                    <td rowspan="2" class="item" style="text-align: center;"><b>N° ordre</b></td>
-                    <td rowspan="2" class="item" style="text-align: center;"><b>CIN</b></td>
-                    <td rowspan="2" class="item" style="text-align: center;"><b>Civilité</b></td>
-                    <td rowspan="2" class="item" style="text-align: center;"><b>Prénom</b></td>
-                    <td rowspan="2" class="item" style="text-align: center;"><b>NOM</b></td>
-                    <td rowspan="2" class="item" style="text-align: center;"><b>Date naissance</b></td>
-                    <td rowspan="2" class="item" style="text-align: center;"><b>Lieu de naissance</b></td>
-                    <td rowspan="2" class="item" style="text-align: center;"><b>Téléphone</b></td>
-                    <td colspan="3" style="text-align: center;"><b>{{ __('DECISION DU JURY') }}</b></td>
-                </tr>
-                <tr class="item" style="text-align: center;">
-                    <td><b>Note</b></td>
-                    <td><b>Niveau de maitrise</b></td>
-                    <td><b>Observations</b></td>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $i = 1; ?>
-                @foreach ($projet->individuelles as $individuelle)
-                    <tr class="item" style="text-align: center;">
-                        <td>{{ $i++ }}</td>
-                        <td>{{ $individuelle->user->cin }}</td>
-                        <td>{{ $individuelle?->user?->civilite }}</td>
-                        <td>{{ format_proper_name($individuelle?->user?->firstname) }}</td>
-                        <td>{{ remove_accents_uppercase($individuelle?->user?->name) }}</td>
-                        <td>{{ $individuelle?->user?->date_naissance?->format('d/m/Y') }}</td>
-                        <td>{{ remove_accents_uppercase($individuelle?->user?->lieu_naissance) }}</td>
-                        <td>{{ $individuelle?->user?->telephone }}</td>
-                        <td>{{ $individuelle?->note_obtenue ?? '' }}</td>
-                        <td>{{ $individuelle?->appreciation }}</td>
-                        <td>{{ $individuelle?->observations }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table> --}}
-
         <table class="table table-responsive">
             <thead>
                 <tr class="heading" style="text-align: center;">
@@ -166,49 +87,24 @@
                 </tr>
 
                 <tr class="heading">
-                    {{-- <td class="item" style="text-align: center;" width="5%"><b>Rang</b></td>
-                    <td class="item" style="text-align: center;" width="12%"><b>CIN</b></td>
-                    <td class="item" style="text-align: center;" width="5%"><b>Civilité</b></td>
-                    <td class="item" style="text-align: center;" width="15%"><b>Prénom</b></td>
-                    <td class="item" style="text-align: center;" width="10%"><b>NOM</b></td>
-                    <td class="item" style="text-align: center;" width="10%"><b>Date naissance</b></td>
-                    <td class="item" style="text-align: center;" width="12%"><b>Lieu de naissance</b></td>
-                    <td class="item" style="text-align: center;"><b>Email</b></td>
-                    <td class="item" style="text-align: center;" width="10%"><b>Téléphone</b></td>
-                    <td class="item" style="text-align: center;"><b>Localité</b></td> --}}
                     <td class="item" style="text-align: center;"><b>Rang</b></td>
-                    <td class="item" style="text-align: center;" width="12%"><b>CIN</b></td>
+                    <td class="item" style="text-align: center;"><b>CIN</b></td>
                     <td class="item" style="text-align: center;"><b>Civilité</b></td>
                     <td class="item" style="text-align: center;"><b>Prénom</b></td>
                     <td class="item" style="text-align: center;"><b>NOM</b></td>
                     <td class="item" style="text-align: center;"><b>Date naissance</b></td>
                     <td class="item" style="text-align: center;"><b>Lieu de naissance</b></td>
-                    <td class="item" style="text-align: center;" width="12%"><b>Email</b></td>
-                    <td class="item" style="text-align: center;" width="10%"><b>Téléphone</b></td>
+                    <td class="item" style="text-align: center;"><b>Téléphone</b></td>
                     <td class="item" style="text-align: center;"><b>Localité</b></td>
                 </tr>
             </thead>
             <tbody>
-                {{-- <?php $i = 1; ?>
-                @forelse($individuelles as $individuelle)
-                    <tr class="item" style="text-align: center;">
-                        <td>{{ $i++ }}</td>
-                        <td>{{ $individuelle->user->cin }}</td>
-                        <td>{{ $individuelle?->user?->civilite }}</td>
-                        <td>{{ format_proper_name($individuelle?->user?->firstname) }}</td>
-                        <td>{{ remove_accents_uppercase($individuelle?->user?->name) }}</td>
-                        <td>{{ $individuelle?->user?->date_naissance?->format('d/m/Y') }}</td>
-                        <td>{{ remove_accents_uppercase($individuelle?->user?->lieu_naissance) }}</td>
-                        <td><span style="color: #007bff;">{{ $individuelle?->user?->email }}</span></td>
-                        <td>{{ $individuelle?->user?->telephone }}</td>
-                        <td>{{ $individuelle->departement->nom }}</td>
-                    </tr>
-                @endforeach --}}
                 @php
                     $rang = 1;
                     $precedenteNote = null;
                     $compteur = 0;
                     $rangAffiche = 1;
+                    $rangLimiteDepasse = false;
 
                     $formatRangFr = function ($rang) {
                         return $rang === 1 ? '1er' : $rang . 'ème';
@@ -219,8 +115,14 @@
                     @php
                         $noteActuelle = $individuelle->note;
 
-                        // Détection de changement de note
+                        // Nouveau rang si note différente
                         if ($noteActuelle !== $precedenteNote) {
+                            // On vérifie si on a dépassé le rang 25
+                            if ($rang > 25) {
+                                $rangLimiteDepasse = true;
+                                break;
+                            }
+
                             $rangAffiche = $rang;
                             $compteur = 1;
                         } else {
@@ -228,14 +130,9 @@
                         }
 
                         $precedenteNote = $noteActuelle;
-
-                        // On arrête après le 25e rang
-                        if ($rangAffiche > 25) {
-                            break;
-                        }
-
                         $rang++;
                     @endphp
+
                     <tr class="item" style="text-align: center;">
                         <td>
                             {{ $formatRangFr($rangAffiche) }}
@@ -249,30 +146,13 @@
                         <td>{{ remove_accents_uppercase($individuelle?->user?->name) }}</td>
                         <td>{{ $individuelle?->user?->date_naissance?->format('d/m/Y') }}</td>
                         <td>{{ remove_accents_uppercase($individuelle?->user?->lieu_naissance) }}</td>
-                        <td><span style="color: #007bff;">{{ $individuelle?->user?->email }}</span></td>
                         <td>{{ $individuelle?->user?->telephone }}</td>
                         <td>{{ $individuelle->departement->nom }}</td>
                     </tr>
                 @endforeach
-
             </tbody>
-        </table>
 
-        {{-- <h4 valign="top">
-            <b><u>SIGNATURE DES MEMBRES DU JURY</u></b> : @isset($formation?->date_pv)
-                <span
-                    style="float: right; font-style: italic">{{ $formation?->departement?->nom . ', ' . $formation?->departement?->region?->nom . ', le ' . $formation?->date_pv?->format('d/m/Y') }}</span>
-            @endisset
-            <br>
-            <?php $i = 1; ?>
-            {{ $formation?->evaluateur?->name . ', ' . $formation?->evaluateur?->fonction }}<br>
-            {{ $formation?->onfpevaluateur?->name . ', ' . $formation?->onfpevaluateur?->fonction }}<br>
-            @isset($membres_jury)
-                @foreach ($membres_jury as $item)
-                    {{ $item }} <br>
-                @endforeach
-            @endisset
-        </h4> --}}
+        </table>
     </div>
 </body>
 
