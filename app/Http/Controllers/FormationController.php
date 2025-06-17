549ae53d176ffdb63da84c95bc6efe19e8398488
elhadji907 <badjilaminefbkk@gmail.com>
2025-06-17 22:10:29 +0000

    update formation

--- app/Http/Controllers/FormationController.php
+++ app/Http/Controllers/FormationController.php
@@ -988,7 +988,7 @@ class FormationController extends Controller
                 ->where('regions.nom', $region->nom)
                 ->where('individuelles.statut', 'Attente')
                 ->get(); */
-            $statutsVoulus = ['attente', 'conforme', 'retiré', 'liste attente', 'Sélectionné'];
+            $statutsVoulus = ['attente', 'conforme', 'retirée','retiré', 'liste attente', 'Sélectionné'];
 
             $individuelles = Individuelle::join('modules', 'modules.id', 'individuelles.modules_id')
                 ->join('regions', 'regions.id', 'individuelles.regions_id')
@@ -1017,7 +1017,7 @@ class FormationController extends Controller
                 ->where('individuelles.statut', 'Attente')
                 ->get(); */
 
-            $statutsVoulus = ['attente', 'conforme', 'retiré', 'liste attente', 'Non conforme', 'Sélectionné'];
+            $statutsVoulus = ['attente', 'conforme', 'retirée','retiré', 'liste attente', 'Sélectionné'];
 
             $individuelles = Individuelle::join('modules', 'modules.id', 'individuelles.modules_id')
                 ->join('regions', 'regions.id', 'individuelles.regions_id')
