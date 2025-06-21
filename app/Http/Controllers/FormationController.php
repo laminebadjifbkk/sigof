a43278a06d5ee99bce592b31db53b88835713cb4
elhadji907 <badjilaminefbkk@gmail.com>
2025-06-21 22:25:40 +0000

    update confirmation formation

--- app/Http/Controllers/FormationController.php
+++ app/Http/Controllers/FormationController.php
@@ -988,7 +988,7 @@ class FormationController extends Controller
                 ->where('regions.nom', $region->nom)
                 ->where('individuelles.statut', 'Attente')
                 ->get(); */
-            $statutsVoulus = ['attente', 'conforme', 'retirée', 'retiré', 'liste attente', 'Sélectionné'];
+            $statutsVoulus = ['attente', 'conforme', 'retirée','retiré', 'liste attente', 'Sélectionné'];
 
             $individuelles = Individuelle::join('modules', 'modules.id', 'individuelles.modules_id')
                 ->join('regions', 'regions.id', 'individuelles.regions_id')
@@ -1017,7 +1017,7 @@ class FormationController extends Controller
                 ->where('individuelles.statut', 'Attente')
                 ->get(); */
 
-            $statutsVoulus = ['attente', 'conforme', 'retirée', 'retiré', 'liste attente', 'Sélectionné'];
+            $statutsVoulus = ['attente', 'conforme', 'retirée','retiré', 'liste attente', 'Sélectionné'];
 
             $individuelles = Individuelle::join('modules', 'modules.id', 'individuelles.modules_id')
                 ->join('regions', 'regions.id', 'individuelles.regions_id')
@@ -1544,8 +1544,6 @@ class FormationController extends Controller
             ->all();
 
         $domaines = Domaine::orderBy("created_at", "desc")->get();
-        
-        dd($idformation, $idlocalite);
 
         return view("formations.collectives.add-collective-modules", compact('formation', 'collectivemodules', 'localite', 'collectivemoduleFormation', 'domaines'));
     }
