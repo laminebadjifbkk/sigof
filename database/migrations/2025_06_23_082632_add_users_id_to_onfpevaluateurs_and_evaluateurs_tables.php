a9479eb9ebf134a302008a99b5a4388cf1d91365
elhadji907 <badjilaminefbkk@gmail.com>
2025-06-23 08:33:26 +0000

    update users_id evaluateurs

---
+++ database/migrations/2025_06_23_082632_add_users_id_to_onfpevaluateurs_and_evaluateurs_tables.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('onfpevaluateurs', function (Blueprint $table) {
+            $table->unsignedBigInteger('users_id')->nullable()->after('id');
+        });
+
+        Schema::table('evaluateurs', function (Blueprint $table) {
+            $table->unsignedBigInteger('users_id')->nullable()->after('id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('onfpevaluateurs', function (Blueprint $table) {
+            $table->dropForeign(['users_id']);
+            $table->dropColumn('users_id');
+        });
+
+        Schema::table('evaluateurs', function (Blueprint $table) {
+            $table->dropForeign(['users_id']);
+            $table->dropColumn('users_id');
+        });
+    }
+};
