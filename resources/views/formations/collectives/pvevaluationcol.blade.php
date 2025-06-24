93d2236aa48e04b9dae6d3148b49d5f4743253bc
elhadji907 <badjilaminefbkk@gmail.com>
2025-06-24 15:43:40 +0000

    update

--- resources/views/formations/collectives/pvevaluationcol.blade.php
+++ resources/views/formations/collectives/pvevaluationcol.blade.php
@@ -208,19 +208,19 @@
                     {{--  <td colspan="7">
                         <b>{{ __('Ingénieur en charge : ') }}</b>{{ $formation?->ingenieur?->name . '(' . $formation?->ingenieur?->initiale . ')' }}
                     </td> --}}
-                    <td rowspan="2" class="item" style="text-align: center; width: 50px;"><b>N°</b></td>
-                    <td rowspan="2" class="item" style="text-align: center; width: 120px;"><b>N° CIN</b></td>
-                    <td rowspan="2" class="item" style="text-align: center; width: 50px;"><b>Civilité</b></td>
+                    <td rowspan="2" class="item" style="text-align: center; width: 5%;"><b>N°</b></td>
+                    <td rowspan="2" class="item" style="text-align: center; width: 12%;"><b>N° CIN</b></td>
+                    <td rowspan="2" class="item" style="text-align: center; width: 5%;"><b>Civilité</b></td>
                     <td rowspan="2" class="item" style="text-align: center;"><b>Prénom</b></td>
                     <td rowspan="2" class="item" style="text-align: center;"><b>NOM</b></td>
-                    <td rowspan="2" class="item" style="text-align: center; width: 80px;"><b>Date naissance</b></td>
+                    <td rowspan="2" class="item" style="text-align: center; width: 12%;"><b>Date naissance</b></td>
                     <td rowspan="2" class="item" style="text-align: center;"><b>Lieu de naissance</b></td>
                     <td rowspan="2" class="item" style="text-align: center; width: 70px;"><b>Téléphone</b></td>
                     <td colspan="3" style="text-align: center;"><b>{{ __('DECISION DU JURY') }}</b>
                     </td>
                 </tr>
                 <tr class="item" style="text-align: center;">
-                    <td style="text-align: center; width: 50px;"><b>Note</b></td>
+                    <td style="text-align: center; width: 5%;"><b>Note</b></td>
                     <td><b>Niveau de maitrise</b></td>
                     <td><b>Observations</b></td>
                 </tr>
