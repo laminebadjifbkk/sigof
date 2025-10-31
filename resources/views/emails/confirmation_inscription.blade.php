cfacda6ee8043473351fc3ecd4ec8bc6495c6ea1
elhadji907 <badjilaminefbkk@gmail.com>
2025-10-31 12:32:36 +0000

    update

--- resources/views/emails/confirmation_inscription.blade.php
+++ resources/views/emails/confirmation_inscription.blade.php
@@ -8,7 +8,26 @@
 
 <body>
     <h2>Bonjour {{ $inscription->nom }},</h2>
-    <p>Merci d’avoir confirmé votre participation à <strong>ONFP - Partnership Engagement Day</strong>.</p>
+    {{-- <p>Merci d’avoir confirmé votre participation à <strong>ONFP - Partnership Engagement Day</strong>.</p> --}}
+    <p>
+        Nous vous remercions d'avoir confirmé votre participation au <b>PARTNERSHIP ENGAGEMENT DAY.</b><br>
+        Vous trouverez ci-joints les termes de références, ainsi qu'une fiche synoptique de quelques projets de l'ONFP
+        (Recherche, Formation, Edition, Construction), qui pourraient potentiellement faire l'objet de partenariat entre
+        nos deux structures.<br>
+        De plus, afin d’identifier vos projets et programmes et de définir provisoirement notre cadre de collaboration,
+        nous vous invitons à bien vouloir renseigner les deux formulaires ci-dessous :<br><br>
+
+        <a href="https://bit.ly/47k1zBX">FICHE D’IDENTIFICATION DES PROJETS ET/OU PROGRAMMES DES PARTENAIRES</a>
+        <br><br>
+        <a href="https://bit.ly/4qAd3Zt">MANIFESTATION D'INTÉRÊT DE PARTENARIAT</a>
+        <br><br>
+        Nous serons ravis de vous rencontrer le <b>06 Novembre 2025 à 08h00, à l’Hôtel AZALAI.</b><br><br>
+        Cordialement, <br><br>
+
+        <b>CELLULE COOPERATION ET PARTENARIAT-ONFP</b> <br><br>
+
+        <b>Contact </b> : <a href="tel:+221772913349">772913349</a> / <a href="tel:+221783753781">783753781</a> <br>
+    </p>
     <p>Voici vos informations :</p>
     <ul>
         <li><b>Structure</b> : {{ $inscription->structure }}</li>
