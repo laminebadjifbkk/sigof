30a3665f571b0e25eb9ce42eef123bae34abd535
elhadji907 <badjilaminefbkk@gmail.com>
2025-11-04 00:19:43 +0000

    update

--- resources/views/layout/user-layout.blade.php
+++ resources/views/layout/user-layout.blade.php
@@ -788,32 +788,30 @@
 
         .déjà {
             background-color: #6c9d78;
-            /* couleur cyan Bootstrap (info) */
+            /* vert doux/grisé */
             color: #ffffff;
             padding: 6px 12px;
             text-align: center;
             border-radius: 12px;
-            /* plus doux et moderne */
             font-weight: 600;
             font-size: 0.95rem;
             box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-            /* légère ombre */
             display: inline-block;
         }
+
         .exécutée {
             background-color: #6c9d78;
-            /* couleur cyan Bootstrap (info) */
+            /* vert doux/grisé */
             color: #ffffff;
             padding: 6px 12px;
             text-align: center;
             border-radius: 12px;
-            /* plus doux et moderne */
             font-weight: 600;
             font-size: 0.95rem;
             box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-            /* légère ombre */
             display: inline-block;
         }
+
         .formés {
             background-color: #198754;
             /* couleur cyan Bootstrap (info) */
