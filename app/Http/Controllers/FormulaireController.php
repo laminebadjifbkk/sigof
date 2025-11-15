fff11394720b347ce06f40a6bf53231b66406b57
elhadji907 <badjilaminefbkk@gmail.com>
2025-11-15 13:12:28 +0000

    update

--- app/Http/Controllers/FormulaireController.php
+++ app/Http/Controllers/FormulaireController.php
@@ -468,7 +468,7 @@ class FormulaireController extends Controller
             'adresse' => 'Adresse',
             'dernier_diplome' => 'Dernier diplôme obtenu',
             'nom_etablissement' => 'Établissement', */
-            /* 'region' => 'Région', */
+            'region' => 'Région',
             'formation' => 'Formation sollicitée',
             /* 'diplome_vise' => 'Diplôme visé',
             'montant_inscription' => 'Montant inscription',
@@ -513,7 +513,7 @@ class FormulaireController extends Controller
             'adresse' => 'Adresse',
             'dernier_diplome' => 'Dernier diplôme obtenu',
             'nom_etablissement' => 'Établissement', */
-            'region' => 'Région',
+            /* 'region' => 'Région', */
             'formation' => 'Formation sollicitée',
             /* 'diplome_vise' => 'Diplôme visé',
             'montant_inscription' => 'Montant inscription',
