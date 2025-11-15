1496e73dedfd20f094e0bab88d46e6dc8b4b3c47
elhadji907 <badjilaminefbkk@gmail.com>
2025-11-15 13:46:36 +0000

    update

--- app/Http/Controllers/FormulaireController.php
+++ app/Http/Controllers/FormulaireController.php
@@ -396,6 +396,11 @@ class FormulaireController extends Controller
 
         $totalFormulaires = number_format($formulaires?->count(), 0, ',', ' ');
 
+        // Regrouper par statut (y compris les null)
+        $groupes = $formulaires->groupBy(function ($item) {
+            return $item->region ?? 'Aucune région';
+        });
+
         $labels = [
             'cin' => 'CIN',
             'civilite' => 'Civilité',
@@ -429,6 +434,7 @@ class FormulaireController extends Controller
         return view('formulaire.index', compact(
             'formulaires',
             'totalFormulaires',
+            'groupes',
             'labels'
         ));
     }
