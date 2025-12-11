6b0bcee7b23696a091c5ac12259b7077b51f9c31
elhadji907 <badjilaminefbkk@gmail.com>
2025-12-11 12:35:33 +0000

    update

--- app/Http/Controllers/FormulaireController.php
+++ app/Http/Controllers/FormulaireController.php
@@ -49,8 +49,8 @@ class FormulaireController extends Controller
     public function store(Request $request)
     {
 
-        Alert::warning('Information', 'Les inscriptions sont clôturées jusqu\'à nouvel ordre.');
-        return redirect()->back();
+        /* Alert::warning('Information', 'Les inscriptions sont clôturées jusqu\'à nouvel ordre.');
+        return redirect()->back(); */
         // Définir la période d'ouverture des inscriptions
         /* // Définir la période d'ouverture des inscriptions
 
