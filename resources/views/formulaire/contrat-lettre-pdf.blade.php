94d3dc56b50e902ab6b08f00a35e40f8c22412be
elhadji907 <badjilaminefbkk@gmail.com>
2025-12-16 19:22:12 +0000

    update

--- resources/views/formulaire/contrat-lettre-pdf.blade.php
+++ resources/views/formulaire/contrat-lettre-pdf.blade.php
@@ -298,7 +298,7 @@
                     implode(
                         ' ',
                         array_map(
-                            fn($line) => nl2br(e(wordwrap($line, 40, "\n", true))),
+                            fn($line) => nl2br(e(wordwrap($line, 30, "\n", true))),
                             explode(
                                 "\n",
                                 ucfirst(substr($formulaire?->nom_etablissement . ' (' . $formulaire?->autre_2 . ')', 0, 410)),
