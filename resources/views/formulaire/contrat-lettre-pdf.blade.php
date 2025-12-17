ad64e64ea54f86b6baa8c833f87d4e68e3e1e7aa
elhadji907 <badjilaminefbkk@gmail.com>
2025-12-17 01:34:39 +0000

    update

--- resources/views/formulaire/contrat-lettre-pdf.blade.php
+++ resources/views/formulaire/contrat-lettre-pdf.blade.php
@@ -364,8 +364,8 @@
             <!-- TITRE CONTRAT -->
             <h3 style="text-align:center;">
                 CONTRAT N°
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <span style=" width:40%;font-size: 10px;">ONFP/DG/DIOF/ss</span>
+               {{--  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <span style=" width:40%;font-size: 10px;">ONFP/DG/DIOF/ss</span> --}}
             </h3>
 
             <p style="text-align:center;"><b>Entre les soussignés</b></p>
