75bfb2dfd6fe8a403dcd24505a8df22f3ec89a43
elhadji907 <badjilaminefbkk@gmail.com>
2025-12-22 17:43:23 +0000

    update

--- resources/views/formulaire/contrat-lettre-pdf.blade.php
+++ resources/views/formulaire/contrat-lettre-pdf.blade.php
@@ -340,16 +340,16 @@
             </div>
 
         </div>
-        {{-- <footer>
-            <div class="page-number" id="footer">
-                <div class="footer-line"></div>
-                <p class="footer-text">Cité Sipres 1, Lot 2 - 2 voies liberté 6 extension VDN
+        <footer>
+            <div class="footer-line"></div>
+
+            <div class="footer-content">
+                <p class="footer-text footer-left">Cité Sipres 1, Lot 2 - 2 voies liberté 6 extension VDN
                     Tel: <a href="tel:+221338279251">33 827 92 51</a> - Fax: 33 827 92 55 <br>
                     BP: 21013 Dakar-Ponty - Email: <a href="mailto:onfp@onfp.sn">onfp@onfp.sn</a>
                 </p>
             </div>
-        </footer> --}}
-        @include('formulaire.footer')
+        </footer>
     </div>
 
     <!-- Saut de page obligatoire -->
@@ -568,9 +568,9 @@
                 </p>
             </div>
         </footer> --}}
-        <!-- FOOTER GLOBAL -->
-        @include('formulaire.footer')
     </div>
+    <!-- FOOTER GLOBAL -->
+    @include('formulaire.footer')
 
 </body>
 
