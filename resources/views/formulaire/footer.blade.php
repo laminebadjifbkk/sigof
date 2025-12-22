e34abfc0f84b67909f91a7f5710902afbe3e7657
elhadji907 <badjilaminefbkk@gmail.com>
2025-12-22 17:25:45 +0000

    update

--- resources/views/formulaire/footer.blade.php
+++ resources/views/formulaire/footer.blade.php
@@ -9,14 +9,20 @@
 </footer>
  --}}
 
-<div class="footer-row">
-    <p class="footer-text footer-left">
-        Cité Sipres 1, Lot 2 - 2 voies liberté 6 extension VDN
-        Tel: 33 827 92 51 - Fax: 33 827 92 55
-        BP: 21013 Dakar-Ponty - Email: onfp@onfp.sn
-    </p>
+<footer>
+    <div class="page-number">
+        <div class="footer-line"></div>
 
-    <p class="footer-text footer-right">
-        Page <span class="page"></span> / <span class="total-pages"></span>
-    </p>
-</div>
+        <div class="footer-row">
+            <p class="footer-text footer-left">
+                Cité Sipres 1, Lot 2 - 2 voies liberté 6 extension VDN
+                Tel: 33 827 92 51 - Fax: 33 827 92 55
+                BP: 21013 Dakar-Ponty - Email: onfp@onfp.sn
+            </p>
+
+            <p class="footer-text footer-right">
+                Page <span class="page"></span> / <span class="total-pages"></span>
+            </p>
+        </div>
+    </div>
+</footer>
