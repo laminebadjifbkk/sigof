dc8d8e0147d3323a8f11be372df412a3b0ad1319
elhadji907 <badjilaminefbkk@gmail.com>
2026-01-02 10:27:08 +0000

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -111,13 +111,13 @@ class UserController extends Controller
             $individuelles = $individuelles->merge($batch);
         });
 
-        dd($individuelles);
-
         // Collectives
         $collectives = collect();
         Collective::select('id')->chunk(300, function ($batch) use (&$collectives) {
             $collectives = $collectives->merge($batch);
         });
+
+        dd($collectives);
 
         // Listecollectives
         $listecollectives = collect();
