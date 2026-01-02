71e6e5c6a1b7dd459d12596c5f9aaec5e7079f04
elhadji907 <badjilaminefbkk@gmail.com>
2026-01-02 10:19:38 +0000

    update

--- resources/views/parc/type-missions/create.blade.php
+++ resources/views/parc/type-missions/create.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
 
         <div class="d-flex justify-content-between align-items-center mb-3">
-            <h1>Ajouter un type de mission</h1>
+            <h3>Ajouter un type de mission</h3>
             <a href="{{ route('parc-type-missions.index') }}" class="btn btn-outline-secondary btn-sm">
                 Retour
             </a>
--- resources/views/parc/type-missions/index.blade.php
+++ resources/views/parc/type-missions/index.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
 
         <div class="d-flex justify-content-between align-items-center mb-3">
-            <h1 class="mb-0">Types de mission</h1>
+            <h3 class="mb-0">Types de mission</h3>
             <a href="{{ route('parc-type-missions.create') }}" class="btn btn-sm btn-primary">
                 <i class="bi bi-plus-circle"></i> Ajouter un type
             </a>
