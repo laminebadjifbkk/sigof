6fa171a9f5b1686c417162ffd35498b7ecd59f56
elhadji907 <badjilaminefbkk@gmail.com>
2026-01-02 15:58:20 +0000

    update

--- resources/views/parc/chauffeurs/show.blade.php
+++ resources/views/parc/chauffeurs/show.blade.php
@@ -5,7 +5,7 @@
     <section class="section register">
         <div class="container">
             <div class="d-flex justify-content-between align-items-center mb-3">
-                <h1 class="mb-0">Détails du chauffeur</h1>
+                <h3 class="mb-0">Détails du chauffeur</h3>
                 <a href="{{ route('parc-chauffeurs.index') }}" class="btn btn-outline-secondary btn-sm">
                     <i class="bi bi-arrow-left-circle"></i> Retour à la liste
                 </a>
