ec6631172a9772715f6d973f69b5537d1c1f1dd0
elhadji907 <badjilaminefbkk@gmail.com>
2026-01-21 09:35:42 +0000

    update

--- resources/views/individuelles/index_annee_region.blade.php
+++ resources/views/individuelles/index_annee_region.blade.php
@@ -30,7 +30,7 @@
                                 <div class="row mb-4">
 
                                     <div class="d-flex justify-content-between align-items-center mb-3">
-                                        <h5>Année {{ $annee }} / {{ $regionNom }}</h5>
+                                        <h5>ANNEE {{ $annee }} / {{ $regionNom }}</h5>
                                         <a href="{{ route('individuelles.parAnnee', ['annee' => $annee]) }}"
                                             class="btn btn-outline-secondary btn-sm">
                                             <i class="bi bi-arrow-left-circle"></i> Retour à la liste
