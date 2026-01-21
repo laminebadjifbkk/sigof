e1a80f397dbfee3d539d2c99093d2193e33fcb60
elhadji907 <badjilaminefbkk@gmail.com>
2026-01-21 09:36:35 +0000

    update

--- resources/views/individuelles/index_annee.blade.php
+++ resources/views/individuelles/index_annee.blade.php
@@ -26,7 +26,7 @@
                     @endif
 
                     <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h5>ANNEE : {{ $annee }}</h5>
+                        <h5>{{ $annee }}</h5>
                         <a href="{{ route('individuelles.index') }}" class="btn btn-outline-secondary btn-sm">
                             <i class="bi bi-arrow-left-circle"></i> Retour à la liste
                         </a>
