efb5f177707ec7fc295946b794bc4aa69a15e1bf
elhadji907 <badjilaminefbkk@gmail.com>
2026-01-22 16:13:09 +0000

    update

--- resources/views/formulaire/show.blade.php
+++ resources/views/formulaire/show.blade.php
@@ -24,7 +24,7 @@
                         <div class="card-header d-flex justify-content-between align-items-center">
                             <h5 class="mb-0">Détails de l’inscription</h5>
                             @can('generer-contrat-lettre-view')
-                                @if ($formulaire->statut === 'Sélectionné')
+                                @if ($formulaire->statut === 'Sélectionné' || $formulaire->statut === 'liste attente')
                                     <a href="{{ route('generer-lalettre.pdf', ['id' => $formulaire->id]) }}"
                                         class="btn btn-outline-danger btn-sm d-inline-flex align-items-center gap-2"
                                         title="Télécharger les contrats et lettres en PDF" target="_blank">
