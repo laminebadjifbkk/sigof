88d339ece3f4e74b0dc31f1b34e1d8d4bb33e41c
elhadji907 <badjilaminefbkk@gmail.com>
2026-01-23 11:32:21 +0000

    update

--- app/Console/Commands/NotifierGroupesVingtDemandeurs.php
+++ app/Console/Commands/NotifierGroupesVingtDemandeurs.php
@@ -99,7 +99,9 @@ class NotifierGroupesVingtDemandeurs extends Command
         }
 
         // Envoyer un seul email
-        Mail::to(['lamine.badji@onfp.sn'])->send(new NotificationDemandeursMail($donnees, $seuil));
+        Mail::to('lamine.badji@onfp.sn')
+            ->cc('mohamadou.soumare@onfp.sn')
+            ->send(new NotificationDemandeursMail($donnees, $seuil));
 
         $this->info("Email récapitulatif envoyé.");
     }
