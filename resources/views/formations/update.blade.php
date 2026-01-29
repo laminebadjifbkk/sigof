7aa6ad0812df9ea500328e597eb967ae2651711b
elhadji907 <badjilaminefbkk@gmail.com>
2026-01-29 19:14:08 +0000

    update

--- resources/views/formations/update.blade.php
+++ resources/views/formations/update.blade.php
@@ -18,7 +18,7 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-sm-12 pt-2">
-                                        <span class="d-flex mt-2 align-items-baseline"><a href="{{ route('formations.index') }}"
+                                        <span class="d-flex mt-2 align-items-baseline"><a href="{{ route('formations.show', $formation) }}"
                                                 class="btn btn-success btn-sm" title="retour"><i
                                                     class="bi bi-arrow-counterclockwise"></i></a>&nbsp;
                                             <p> | Formations</p>
