b98c120763b4542cdaee4ecab47a0668e4b0b8dc
elhadji907 <badjilaminefbkk@gmail.com>
2026-02-26 15:56:57 +0000

    update

--- resources/views/employes/index.blade.php
+++ resources/views/employes/index.blade.php
@@ -42,102 +42,106 @@
                         </a>
                         <h5 class="card-title">Liste des employés</h5>
                         @if ($employes->isNotEmpty())
-                            <table class="table datatables align-middle" id="table-employes">
-                                <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th>Matricule</th>
-                                        <th>Prénom & NOM</th>
-                                        <th>E-mail</th>
-                                        <th>Téléphone</th>
-                                        <th>Direction</th>
-                                        <th>#</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php $i = 1; ?>
-                                    @foreach ($employes as $employe)
+                            <div class="table-responsive">
+                                <table class="table datatables align-middle" id="table-employes">
+                                    <thead>
                                         <tr>
-                                            <th scope="row">
-                                                {{-- <a href="#" data-bs-toggle="modal"
+                                            <th></th>
+                                            <th>Matricule</th>
+                                            <th>Prénom & NOM</th>
+                                            <th>E-mail</th>
+                                            <th>Téléphone</th>
+                                            <th>Direction</th>
+                                            <th>#</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php $i = 1; ?>
+                                        @foreach ($employes as $employe)
+                                            <tr>
+                                                <th scope="row">
+                                                    {{-- <a href="#" data-bs-toggle="modal"
                                                     data-bs-target="#ShowIMG{{ $employe?->user?->id }}">
                                                     <img class="rounded-circle w-40" alt="Profil"
                                                         src="{{ asset($employe?->user?->getImage()) }}" width="40"
                                                         height="auto">
                                                 </a> --}}
 
-                                                <a href="#" data-bs-toggle="modal"
-                                                    data-bs-target="#ShowIMG{{ $employe?->user?->id }}"
-                                                    class="table-profile-image-wrapper
+                                                    <a href="#" data-bs-toggle="modal"
+                                                        data-bs-target="#ShowIMG{{ $employe?->user?->id }}"
+                                                        class="table-profile-image-wrapper
                                                 {{ $employe?->user?->last_activity &&
                                                 \Carbon\Carbon::parse($employe?->user?->last_activity)->diffInMinutes(now()) < 5
                                                     ? 'online'
                                                     : 'offline' }}">
-                                                    <img src="{{ asset($employe?->user?->getImage()) }}"
-                                                        class="rounded-circle w-20" alt="Profil"
-                                                        class="table-profile-image" width="40" height="auto">
-                                                </a>
-                                            </th>
-                                            {{-- <td>{{ $i++ }}</td> --}}
-                                            <td>{{ $employe?->matricule }}</td>
-                                            <td>{{ implode(' ', array_filter([$employe?->user?->civilite, $employe?->user?->firstname, $employe?->user?->name])) }}
-                                            </td>
-                                            <td><a
-                                                    href="mailto:{{ $employe?->user?->email }}">{{ $employe?->user?->email }}</a>
-                                            </td>
-                                            <td><a
-                                                    href="tel:+221{{ $employe?->user?->telephone }}">{{ $employe?->user?->telephone }}</a>
-                                            </td>
-                                            <td>{{ $employe?->direction?->name }}</td>
-                                            <td>
+                                                        <img src="{{ asset($employe?->user?->getImage()) }}"
+                                                            class="rounded-circle w-20" alt="Profil"
+                                                            class="table-profile-image" width="40" height="auto">
+                                                    </a>
+                                                </th>
+                                                {{-- <td>{{ $i++ }}</td> --}}
+                                                <td>{{ $employe?->matricule }}</td>
+                                                <td>{{ implode(' ', array_filter([$employe?->user?->civilite, $employe?->user?->firstname, $employe?->user?->name])) }}
+                                                </td>
+                                                <td><a
+                                                        href="mailto:{{ $employe?->user?->email }}">{{ $employe?->user?->email }}</a>
+                                                </td>
+                                                <td><a
+                                                        href="tel:+221{{ $employe?->user?->telephone }}">{{ $employe?->user?->telephone }}</a>
+                                                </td>
+                                                <td>{{ $employe?->direction?->name }}</td>
+                                                <td>
 
-                                                @can('employe-show')
-                                                    <span class="d-flex mt-2 align-items-baseline"><a
-                                                            href="{{ route('employes.show', $employe) }}"
-                                                            class="btn btn-success btn-sm mx-1" title="voir détails"><i
-                                                                class="bi bi-eye"></i></a>
-                                                        <div class="filter">
-                                                            <a class="icon" href="#" data-bs-toggle="dropdown"><i
-                                                                    class="bi bi-three-dots"></i></a>
-                                                            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                                    @can('employe-show')
+                                                        <span class="d-flex mt-2 align-items-baseline"><a
+                                                                href="{{ route('employes.show', $employe) }}"
+                                                                class="btn btn-success btn-sm mx-1" title="voir détails"><i
+                                                                    class="bi bi-eye"></i></a>
+                                                            <div class="filter">
+                                                                <a class="icon" href="#" data-bs-toggle="dropdown"><i
+                                                                        class="bi bi-three-dots"></i></a>
+                                                                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
 
-                                                                @can('employe-update')
-                                                                    <li><a class="dropdown-item btn btn-sm mx-1"
-                                                                            href="{{ route('employes.edit', $employe) }}"
-                                                                            class="mx-1"><i class="bi bi-pencil"></i> Modifier</a>
-                                                                    </li>
-                                                                @endcan
+                                                                    @can('employe-update')
+                                                                        <li><a class="dropdown-item btn btn-sm mx-1"
+                                                                                href="{{ route('employes.edit', $employe) }}"
+                                                                                class="mx-1"><i class="bi bi-pencil"></i>
+                                                                                Modifier</a>
+                                                                        </li>
+                                                                    @endcan
 
-                                                                @can('employe-delete')
-                                                                    <li>
-                                                                        <form action="{{ route('employes.destroy', $employe) }}"
-                                                                            method="post">
-                                                                            @csrf
-                                                                            @method('DELETE')
-                                                                            <button type="submit"
-                                                                                class="dropdown-item show_confirm"><i
-                                                                                    class="bi bi-trash"></i>Supprimer</button>
-                                                                        </form>
-                                                                    </li>
-                                                                @endcan
-                                                            </ul>
-                                                        </div>
-                                                    </span>
-                                                @endcan
-                                            </td>
-                                        </tr>
-                                    @endforeach
+                                                                    @can('employe-delete')
+                                                                        <li>
+                                                                            <form
+                                                                                action="{{ route('employes.destroy', $employe) }}"
+                                                                                method="post">
+                                                                                @csrf
+                                                                                @method('DELETE')
+                                                                                <button type="submit"
+                                                                                    class="dropdown-item show_confirm"><i
+                                                                                        class="bi bi-trash"></i>Supprimer</button>
+                                                                            </form>
+                                                                        </li>
+                                                                    @endcan
+                                                                </ul>
+                                                            </div>
+                                                        </span>
+                                                    @endcan
+                                                </td>
+                                            </tr>
+                                        @endforeach
 
-                                </tbody>
-                            </table>
-                            <!-- End Table with stripped rows -->
-                        @else
-                            <div class="alert alert-info">Aucun employé pour l'instant !</div>
+                                    </tbody>
+                                </table>
+                                <!-- End Table with stripped rows -->
+                            @else
+                                <div class="alert alert-info">Aucun employé pour l'instant !</div>
                         @endif
                     </div>
                 </div>
-
             </div>
+
+        </div>
         </div>
         @foreach ($employes as $employe)
             <div class="modal fade" id="ShowIMG{{ $employe?->user?->id }}" tabindex="-1" aria-hidden="true">
