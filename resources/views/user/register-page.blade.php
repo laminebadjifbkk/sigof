e16fa0ef73fd96791996f8e0da637eed083b5a9a
elhadji907 <badjilaminefbkk@gmail.com>
2026-03-05 14:17:53 +0000

    update

--- resources/views/user/register-page.blade.php
+++ resources/views/user/register-page.blade.php
@@ -337,7 +337,7 @@
             }
         });
     </script>
-    <script>
+    {{-- <script>
         document.addEventListener("DOMContentLoaded", function() {
             var telephoneInput = document.getElementById("votre_telephone");
 
@@ -352,7 +352,7 @@
                 e.target.value = value.slice(0, 12); // Limite à 12 caractères (avec les ":")
             });
         });
-    </script>
+    </script> --}}
 
 </body>
 
