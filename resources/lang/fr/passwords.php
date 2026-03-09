63c8cf48018a242e069cb34300542041b6434b8d
elhadji907 <badjilaminefbkk@gmail.com>
2026-03-09 22:39:05 +0000

    update

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -15,6 +15,7 @@ return [
     'password' => 'Les mots de passe doivent contenir au moins six caractères et être identiques.',
     'reset'    => 'Votre mot de passe a été réinitialisé !',
     'sent'     => 'Nous vous avons envoyé par courriel le lien de réinitialisation du mot de passe !',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
     'token'    => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
     'user'     => "Aucun utilisateur n'a été trouvé avec cette adresse courriel.",
 ];
