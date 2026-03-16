1e687a6b7fe904e93277919fd86ec7a123de673b
elhadji907 <badjilaminefbkk@gmail.com>
2026-03-16 11:19:54 +0000

    update

--- resources/views/layout/user-layout.blade.php
+++ resources/views/layout/user-layout.blade.php
@@ -2655,8 +2655,7 @@
         }
     </style>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
 
     <script>
         function confirmProfil(profil) {
