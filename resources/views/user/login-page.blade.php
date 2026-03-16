1189a33b9927aa16d2762f6d35b5df1a6b86f8ce
elhadji907 <badjilaminefbkk@gmail.com>
2026-03-16 09:56:07 +0000

    update

--- resources/views/user/login-page.blade.php
+++ resources/views/user/login-page.blade.php
@@ -97,13 +97,25 @@
                                             alt="Google" style="width:20px; height:20px; margin-right:8px;">
                                         Se connecter avec Google
                                     </a> --}}
-                                    <div class="d-flex justify-content-center mt-3">
+                                    {{-- <div class="d-flex justify-content-center mt-3">
                                         <a href="{{ url('auth/google') }}"
                                             class="btn btn-light d-flex align-items-center justify-content-center border shadow-sm mb-3"
                                             style="background-color: #fff; width: 50px; height: 50px; padding: 0;"
                                             title="Se connecter avec Google">
                                             <img src="https://www.gstatic.com/images/branding/product/1x/gsa_64dp.png"
                                                 alt="Google" style="width:24px; height:24px;">
+                                        </a>
+                                    </div> --}}
+                                    <div class="d-flex justify-content-center mt-3">
+                                        <a href="{{ url('auth/google') }}"
+                                            class="btn btn-light d-flex align-items-center gap-2 border shadow-sm px-3 py-2"
+                                            style="background-color:#fff;" title="Se connecter avec Google">
+
+                                            <img src="https://www.gstatic.com/images/branding/product/1x/gsa_64dp.png"
+                                                alt="Google" style="width:20px; height:20px;">
+
+                                            <span>Se connecter avec Google</span>
+
                                         </a>
                                     </div>
 
