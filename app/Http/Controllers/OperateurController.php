d60c7c1368b6b83d313e1086d001d617e8b2a5bb
elhadji907 <badjilaminefbkk@gmail.com>
2026-03-18 16:27:21 +0000

    update

--- app/Http/Controllers/OperateurController.php
+++ app/Http/Controllers/OperateurController.php
@@ -753,7 +753,7 @@ class OperateurController extends Controller
             "numero_arrive"        => ['nullable', 'string', Rule::unique(Operateur::class)->ignore($operateur?->id)->whereNull('deleted_at')],
             "numero_agrement"      => ['nullable', 'string', Rule::unique(Operateur::class)->ignore($operateur?->id)->whereNull('deleted_at')],
             "operateur"            => ['required', 'string', Rule::unique(User::class)->ignore($user->id)->whereNull('deleted_at')],
-            "username"             => ['required', 'string', Rule::unique('users', 'username')
+            "username"             => ['nullable', 'string', Rule::unique('users', 'username')
                 ->ignore($user->id)
                 ->where(fn($query) => $query->whereNull('deleted_at'))],
             "email"                => ['required', 'string', Rule::unique(User::class)->ignore($user->id)->whereNull('deleted_at')],
