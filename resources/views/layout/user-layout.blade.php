afe61546706fe0d27066e9548d0a4c5c81a5a479
elhadji907 <badjilaminefbkk@gmail.com>
2026-03-19 11:48:06 +0000

    update

--- resources/views/layout/user-layout.blade.php
+++ resources/views/layout/user-layout.blade.php
@@ -1945,10 +1945,6 @@
             color: #ffc107;
         }
 
-        .attente {
-            color: #6C757D;
-        }
-
         /* Permis */
         .permis-expire {
             color: #dc3545;
