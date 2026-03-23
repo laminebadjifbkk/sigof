25d911cb107a9b8c91c5283663bd41322abbcd70
elhadji907 <badjilaminefbkk@gmail.com>
2026-03-23 11:10:03 +0000

    update

--- resources/views/formations/index.blade.php
+++ resources/views/formations/index.blade.php
@@ -690,14 +690,12 @@
 @push('scripts')
     <script>
         new DataTable('#table-formations', {
+            ordering: false,
             layout: {
                 topStart: {
                     buttons: ['excel', 'pdf', 'print'],
                 }
             },
-            "order": [
-                [0, 'desc']
-            ],
             language: {
                 "sProcessing": "Traitement en cours...",
                 "sSearch": "Rechercher&nbsp;:",
