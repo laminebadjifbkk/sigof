fa0019a3479b4feafd71615d7333a3bcaffc4b3b
elhadji907 <badjilaminefbkk@gmail.com>
2026-03-23 17:31:33 +0000

    update

--- resources/views/collectives/fiche.blade.php
+++ resources/views/collectives/fiche.blade.php
@@ -176,9 +176,9 @@
                     </tr>
                     <tr>
                         <th>Adresse de la structure</th>
-                        <td colspan="3">{{ $collective?->adresse }}</td>
-                        <td colspan="3">{{ $collective?->departement?->nom }}</td>
-                        <td colspan="3">{{ $collective?->departement?->region?->nom }}</td>
+                        <td colspan="2">{{ $collective?->adresse }}</td>
+                        <td colspan="4">{{ $collective?->departement?->nom }}</td>
+                        <td colspan="4">{{ $collective?->departement?->region?->nom }}</td>
                     </tr>
                     <tr>
                         <th>Contact</th>
