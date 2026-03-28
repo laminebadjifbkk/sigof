65e3de80d9e370534f792aea5b200061747cd37e
elhadji907 <badjilaminefbkk@gmail.com>
2026-03-28 01:26:43 +0000

    update

--- app/Http/Controllers/CommissionagrementController.php
+++ app/Http/Controllers/CommissionagrementController.php
@@ -315,7 +315,7 @@ class CommissionagrementController extends Controller
         $commissionagrement = Commissionagrement::findOrFail($id);
 
         /* $statutsVoulus = ['Conforme', 'Extension', 'Renouvellement', 'Nouveau', 'À corriger', 'agréé', 'sous réserve', 'Rejeté', 'Retiré']; */
-        $statutsVoulus = ['agréé'];
+        $statutsVoulus = ['Nouveau', 'Conforme'];
 
         /* $operateurs = Operateur::whereNull('commissionagrements_id')
             ->whereIn('statut_agrement', $statutsVoulus)
