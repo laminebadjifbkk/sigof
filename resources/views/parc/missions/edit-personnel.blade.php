e7c5a902146dff24663d735d37f9eb5b4606f85d
elhadji907 <badjilaminefbkk@gmail.com>
2026-05-02 00:16:19 +0000

    update

--- resources/views/parc/missions/edit-personnel.blade.php
+++ resources/views/parc/missions/edit-personnel.blade.php
@@ -7,7 +7,7 @@
 
             {{-- En-tête --}}
             <div class="d-flex justify-content-between align-items-center mb-3">
-                <h3 class="mb-0">Personnel de la mission : {{ $mission->reference }}</h3>
+                <h3 class="mb-0">Personnel de la mission : {{ $mission->reference }} / {{ $mission->id }}</h3>
                 <a href="{{ route('parc-missions.index') }}" class="btn btn-outline-secondary btn-sm">
                     <i class="bi bi-arrow-left-circle"></i> Retour à la liste
                 </a>
