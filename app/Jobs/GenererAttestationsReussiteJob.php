bdd4ca5006ef6c2e1037257eee30aa1e8ef8dc5d
elhadji907 <badjilaminefbkk@gmail.com>
2026-06-13 15:37:20 +0000

    update

--- app/Jobs/GenererAttestationsReussiteJob.php
+++ app/Jobs/GenererAttestationsReussiteJob.php
@@ -150,7 +150,7 @@ class GenererAttestationsReussiteJob implements ShouldQueue
             }
 
             // Stockage final dans storage/app/public/attestations/
-            $finalName = 'attestations_' . $this->formationId . '_' . Str::slug($formation->name) . '.pdf';
+            $finalName = 'attestations_' . $this->formationId . '_' . Str::slug($formation?->intitule) . '.pdf';
             $finalPath = storage_path('app/public/attestations/' . $finalName);
 
             if (!is_dir(dirname($finalPath))) {
