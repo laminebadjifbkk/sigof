3061b1d75b3cd6dd3faca2e0b8430464361689be
elhadji907 <badjilaminefbkk@gmail.com>
2026-06-15 13:57:56 +0000

    update

--- resources/views/formations/collectives/attestation_reussite.blade.php
+++ resources/views/formations/collectives/attestation_reussite.blade.php
@@ -324,6 +324,7 @@
             margin-top: -0.5mm;
             letter-spacing: 0.5px;
             /* réduire si nécessaire */
+            font-weight: bold;
         }
 
         .qr-zone p:last-child {
@@ -425,7 +426,7 @@
         {{-- QR code en bas à gauche --}}
         <div class="qr-zone">
             <img src="data:image/png;base64,{{ $qrCodeBase64 }}" alt="QR Code">
-            <p class="numero-attestation">{{ $listecollective?->numero_attestation }}</p>
+            <p class="numero-attestation"><strong>{{ $listecollective?->numero_attestation }}</strong></p>
             <p>Vérifier l'authenticité</p>
         </div>
 
--- resources/views/formations/individuelles/attestation_reussite.blade.php
+++ resources/views/formations/individuelles/attestation_reussite.blade.php
@@ -327,6 +327,7 @@
 
             letter-spacing: 0.5px;
             /* réduire si nécessaire */
+            font-weight: bold;
         }
 
         .qr-zone p:last-child {
@@ -428,7 +429,7 @@
         {{-- QR code en bas à gauche --}}
         <div class="qr-zone">
             <img src="data:image/png;base64,{{ $qrCodeBase64 }}" alt="QR Code">
-            <p class="numero-attestation">{{ $individuelle?->numero_attestation }}</p>
+            <p class="numero-attestation"><strong>{{ $individuelle?->numero_attestation }}</strong></p>
             <p>Vérifier l'authenticité</p>
         </div>
 
