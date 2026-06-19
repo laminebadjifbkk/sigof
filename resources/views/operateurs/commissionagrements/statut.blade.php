743f5d5203db5b8c6364375e40ece4177de4057f
elhadji907 <badjilaminefbkk@gmail.com>
2026-06-19 19:30:29 +0000

    update

--- resources/views/operateurs/commissionagrements/statut.blade.php
+++ resources/views/operateurs/commissionagrements/statut.blade.php
@@ -121,7 +121,7 @@
                                                 <th width="5%" class="text-center">Dossier</th>
                                                 <th width="15%">N° agrément</th>
                                                 <th width="50%">Opérateurs</th>
-                                                <th width="10%">Sigle</th>
+                                                {{-- <th width="10%">Sigle</th> --}}
                                                 <th class="text-center">Modules</th>
                                                 <th width="15%" class="text-center">Statut</th>
                                                 <th><i class="bi bi-gear"></i></th>
@@ -134,8 +134,8 @@
                                                 <tr>
                                                     <td class="text-center">{{ $operateur?->numero_dossier }}</td>
                                                     <td>{{ $operateur?->numero_agrement }}</td>
-                                                    <td>{{ $operateur?->user?->operateur }}</td>
-                                                    <td>{{ $operateur?->user?->username }}</td>
+                                                    <td>{{ $operateur?->user?->display_operateur }}</td>
+                                                    {{-- <td>{{ $operateur?->user?->username }}</td> --}}
                                                     {{-- <td style="text-align: center;">
                                                         @foreach ($operateur?->operateurmodules as $operateurmodule)
                                                             @if ($loop->last)
