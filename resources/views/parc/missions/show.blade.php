abdd5dcc1a5bdaa35974f8fa9de80deeb6cd03bc
elhadji907 <badjilaminefbkk@gmail.com>
2026-06-22 20:40:02 +0000

    update

--- resources/views/parc/missions/show.blade.php
+++ resources/views/parc/missions/show.blade.php
@@ -133,7 +133,7 @@
                                         {{ $mission?->creator ? $mission->creator->firstname . ' ' . $mission->creator->name : 'Fatou Boro DIOP' }}
                                         <br>
                                         <small class="text-muted">
-                                            Créée le : {{ $mission->created_at?->translatedFormat('d F Y à H:i') }} <br>
+                                            Le {{ $mission->created_at?->translatedFormat('d F Y à H:i') }} <br>
                                             Dernière modification :
                                             {{ $mission->updated_at?->translatedFormat('d F Y à H:i') }}
                                         </small>
