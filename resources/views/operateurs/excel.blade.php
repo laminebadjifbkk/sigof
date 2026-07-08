ae1049a613dae6002d224a7416c3ebb14581f955
elhadji907 <badjilaminefbkk@gmail.com>
2026-07-08 14:51:10 +0000

    update

--- resources/views/operateurs/excel.blade.php
+++ resources/views/operateurs/excel.blade.php
@@ -26,7 +26,7 @@
             @foreach ($modules as $index => $module)
                 <tr>
                     @if ($index === 0)
-                        <td rowspan="{{ $rowspan }}">{{ $loop->iteration }}</td>
+                        <td rowspan="{{ $rowspan }}">{{ $loop->parent->iteration }}</td>
                         <td rowspan="{{ $rowspan }}">{{ $op?->user?->display_operateur }}</td>
                         <td rowspan="{{ $rowspan }}">{{ $op?->user?->adresse }}</td>
                         <td rowspan="{{ $rowspan }}">{{ $op?->user?->email }}</td>
