ee0e1d18d95374b82f90c3d3646531907f5eed26
elhadji907 <badjilaminefbkk@gmail.com>
2026-07-14 12:59:37 +0000

    update

--- resources/views/formations/individuelles/pvevaluation-vierge.blade.php
+++ resources/views/formations/individuelles/pvevaluation-vierge.blade.php
@@ -283,7 +283,7 @@
                         <td rowspan="2" class="item" style="text-align: center;"><b>Prénom</b></td>
                         <td rowspan="2" class="item" style="text-align: center;"><b>NOM</b></td>
                         <td rowspan="2" class="item" style="text-align: center; width: 10%;"><b>Date nais.</b></td>
-                        <td rowspan="2" class="item" style="text-align: center; width: 20%;"><b>Lieu naissance</b></td>
+                        <td rowspan="2" class="item" style="text-align: center; width: 17%;"><b>Lieu naissance</b></td>
                         <td rowspan="2" class="item" style="text-align: center; width: 10%;"><b>Téléphone</b></td>
                         <td colspan="3" style="text-align: center;"><b>{{ __('DECISION DU JURY') }}</b>
                         </td>
