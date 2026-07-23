b831efd46a1b0c0168a382f4ab42e09f317a2a2d
elhadji907 <badjilaminefbkk@gmail.com>
2026-07-23 23:47:30 +0000

    update

--- app/Http/Controllers/FormationController.php
+++ app/Http/Controllers/FormationController.php
@@ -5410,7 +5410,7 @@ class FormationController extends Controller
         }
 
         dd(Formation::whereNull('ingenieurs_id')
-            ->whereBetween('date_debut', ['2026-11-01 00:00:00', '2026-12-31 23:59:59'])
+            ->whereBetween('date_debut', ['2025-11-01 00:00:00', '2025-12-31 23:59:59'])
             ->count());
 
         $formations = $query->get();
