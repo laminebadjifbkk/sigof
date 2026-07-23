75f3cfc90bd48c754d690b716a14342764d77386
elhadji907 <badjilaminefbkk@gmail.com>
2026-07-23 23:55:24 +0000

    update

--- app/Http/Controllers/FormationController.php
+++ app/Http/Controllers/FormationController.php
@@ -5403,8 +5403,14 @@ class FormationController extends Controller
 
         if ($request->filled('ingenieur')) {
             if ($request->ingenieur === 'null') {
+                $query->whereNull('ingenieurs_id');
             } else {
                 $query->where('ingenieurs_id', $request->ingenieur);
+
+                $ingenieur = Ingenieur::find($request->ingenieur);
+                if ($ingenieur && $ingenieur->name !== null) {
+                    $title .= ' - INGÉNIEUR : ' . strtoupper($ingenieur->name);
+                }
             }
         }
 
