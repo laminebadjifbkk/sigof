963c82eeca75a121d51ef4b0610dbda140e2fa92
elhadji907 <badjilaminefbkk@gmail.com>
2026-07-23 23:55:41 +0000

    update

--- app/Http/Controllers/FormationController.php
+++ app/Http/Controllers/FormationController.php
@@ -5403,7 +5403,7 @@ class FormationController extends Controller
 
         if ($request->filled('ingenieur')) {
             if ($request->ingenieur === 'null') {
-                $query->whereNull('ingenieurs_id');
+                /* $query->whereNull('ingenieurs_id'); */
             } else {
                 $query->where('ingenieurs_id', $request->ingenieur);
 
