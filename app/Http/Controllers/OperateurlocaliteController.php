bc9c4aba451586512d7fca8a76bb2d59ccc456f8
elhadji907 <badjilaminefbkk@gmail.com>
2026-07-24 11:30:27 +0000

    update

--- app/Http/Controllers/OperateurlocaliteController.php
+++ app/Http/Controllers/OperateurlocaliteController.php
@@ -87,7 +87,7 @@ class OperateurlocaliteController extends Controller
         ]);
 
         $operateurlocalite = Operateurlocalite::findOrFail($id);
-        if (! in_array($operateurlocalite->operateur->statut_agrement, ['Nouveau', 'Extension', 'Renouvellement', 'À corriger'])) {
+        if (! in_array($operateurlocalite->operateur->statut_agrement, ['Nouveau', 'Extension', 'Renouvellement', 'À corriger', 'sous réserve'])) {
             Alert::warning('Attention ! ', 'Action impossible');
             return redirect()->back();
         }
