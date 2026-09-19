2224c0b2d5343d6b7aa0247781be6eeb76110fcd
elhadji907 <badjilaminefbkk@gmail.com>
2026-09-19 14:54:33 +0000

    tiers

--- resources/views/layout/page-sidebar.blade.php
+++ resources/views/layout/page-sidebar.blade.php
@@ -167,7 +167,7 @@
                     <li class="nav-item">
                         <a class="nav-link {{ request()->routeIs('onfp.tiers.*') ? 'active' : 'collapsed' }}"
                             href="{{ route('onfp.tiers.index') }}">
-                            <span>Tiers</span>
+                            <span>Tiers intervenants</span>
                         </a>
                     </li>
 
